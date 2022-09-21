a0d1e04c2c9f2bce534f44fbe054e8d80db1d3c9
janghoikz <101626287+janghoikz@users.noreply.github.com>
2022-09-22 06:45:11 +0900

    초기 커밋.

---
+++ DummyClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+//더미 클라
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipaddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipaddr, 7777);
+
+            //휴대폰 설정
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                //문지기한테 입장문의
+                socket.Connect(endPoint);
+                Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
+
+                //보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
+                int sendBytes = socket.Send(sendBuff);
+                //받는다
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Sever]{recvData}");
+                // 나간다
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ SeverCore/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipaddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipaddr, 7777);
+
+            //문지기
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+
+                //문지기 교육
+                listenSocket.Bind(endPoint);
+
+                //영업 시작
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+                    //손님을 입장시킨다
+                    Socket clientSocket = listenSocket.Accept();
+
+                    //받는다
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Client]{recvData}");
+
+                    //보낸다
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcom Server");
+                    clientSocket.Send(sendBuff);
+
+                    //쫓아낸다
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
+        }
+    }
+}
